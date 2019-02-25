07bf96d5606659ac172dea9538f3820427682867
sayan goswami <sayan.goswami@wipro.com>
2019-02-25 14:04:35 +0530

    test changes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test-home', function(req, res, next) {
+  res.send('this is the home page');
+});
+
 module.exports = router;
