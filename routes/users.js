b3f8bdcd39b47aec5e3d6dd76ffed30dff09acb8
sayan goswami <sayan.goswami@wipro.com>
2019-04-22 13:37:36 +0530

    new changes

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const HackApi = require('../service/HackApi');
 const User = require('../models/user.model');
 const _ = require("lodash");
 const jwt = require("../config/jwt-module");
+const nodemailer = require("nodemailer");
 
 /* Login */
 router.post('/login', function (req, res, next) {
@@ -69,6 +70,10 @@ router.post('/admin/login', function (req, res, next){
   });   
 }); 
 
+router.get('/mail',function(req, res, next){
+    sendEMail();
+});
+
 //Check to make sure header is not undefined, if so, return Forbidden (403)
 function checkToken (req, res, next)  {
   const header = req.headers.authorization;
@@ -109,6 +114,47 @@ function checkAdmin (req, res, next) {
   }
   console.log("Failed");
   res.sendStatus(403);
+}
+
+async function sendEMail (req, res, next){
+  try{
+
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    proxy: 'http://proxy1.wipro.com',
+    auth: {
+      user: 'secretsayan@gmail.com', // generated ethereal user
+      pass: 'Hadoop@10' // generated ethereal password
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <secretsayan@gmail.com>', // sender address
+    to: "sayan.goswami@wipro.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  res.json("Hello Mail");
+}catch(e){
+  console.log(e);
+}
+  
+
+
 }
 
 module.exports = router;
