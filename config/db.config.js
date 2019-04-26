ec6b3180fb8adfff89b0107deb1bfa7a684bb07e
secretsayan <sayan.goswami@wipro.com>
2019-04-26 17:21:14 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:'mongodb+srv://root:root@cluster0-zuabs.mongodb.net/test?retryWrites=true'
+    url:'mongodb+srv://root:root@cluster0-0zctk.mongodb.net/test?retryWrites=true'
 };
