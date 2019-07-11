3a06fe4eb9efb40c2bea44880bfd3494be997417
sayan goswami <sayan.goswami@wipro.com>
2019-07-11 16:21:08 +0530

    dbconfig

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-   url:'mongodb+srv://root:root@cluster0-0zctk.mongodb.net/test?retryWrites=true'
+   //url:'mongodb+srv://root:root@cluster0-0zctk.mongodb.net/test?retryWrites=true'
    // url: 'mongodb://localhost:27017/hack'
+   url:'mongodb+srv://root:root@cluster0-zuabs.mongodb.net/test?retryWrites=true&w=majority'
 };
