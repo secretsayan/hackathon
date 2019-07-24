196d009c0f626bb4223c41072b65625ef20c58df
sayan goswami <sayan.goswami@wipro.com>
2019-07-24 20:42:05 +0530

    Jest schema validation changes

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,3 +1,6 @@
+var jsonSchema =  require('jest-json-schema'); 
+expect.extend(jsonSchema.matchers);
+
 const router = require('../routes/events');
 const app = require('../app.js');
 const request =  require('supertest');
@@ -12,5 +15,57 @@ describe('testing events all', () => {
     test('time check', async ()=>{
         const response = await request(app).get('/event/all');
         expect(response).toBeDefined();
+    });
+
+    test('testing schema', async function(){
+        const response = await request(app).get('/event/all');
+        const schema = {
+            required: [
+                'eventId',
+                '_id',
+                'name',
+                'description',
+                'startDate',
+                'endDate',
+                'status',
+                'noOfTeams',
+                'maxTeamSize',
+                'prizes',
+                'hackathonHost',                
+            ],
+            properties:{
+                'eventId':{
+                    'type': 'integer',
+                },
+                '_id':{
+                    'type': 'string',
+                },
+                'name':{
+                    'type': 'string',
+                },
+                'description':{
+                    'type': 'string',
+                },
+                'startDate':{
+                    'type': 'string',
+                },
+                'endDate':{
+                    'type': 'string',
+                },
+                'status':{
+                    'type': 'string',
+                },
+                'noOfTeams':{
+                    'type': 'integer',
+                },
+                'maxTeamSize':{
+                    'type': 'integer',
+                },
+                'prizes':{
+                    'type': 'integer',
+                },
+            }
+        };
+        expect(response.schema).toMatchSchema(schema);
     });
 });
