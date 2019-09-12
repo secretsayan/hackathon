cdafdd71bb77c4ea227753c7841e9cdb32f8d35d
sayan goswami <sayan.goswami@wipro.com>
2019-09-12 20:37:25 +0530

    Rebuild

--- public/main.js
+++ public/main.js
@@ -1107,7 +1107,7 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 var EventsService = /** @class */ (function () {
     function EventsService(_http) {
         this._http = _http;
-        this._eventsUrl = 'http://localhost:3000/event';
+        this._eventsUrl = 'event';
         this.count = 100;
         this.httpOptions = {
             headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpHeaders"]({
@@ -1303,7 +1303,7 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 var RegistrationService = /** @class */ (function () {
     function RegistrationService(_http) {
         this._http = _http;
-        this._regUrl = "http://localhost:3000/reg";
+        this._regUrl = "reg";
         this.count = 100;
         this.httpOptions = {
             headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpHeaders"]({
@@ -1897,7 +1897,7 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 var UsersService = /** @class */ (function () {
     function UsersService(_http) {
         this._http = _http;
-        this._usersUrl = "http://localhost:3000/user";
+        this._usersUrl = "user";
         this.httpOptions = {
             headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_4__["HttpHeaders"]({
                 'Content-Type': 'application/json'
